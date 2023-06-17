a3b542eb1132fa856e46a1799cb6105b9db01270
Omid-Bakhshour <fishvardroid@gmail.com>
2023-06-17 14:01:32 +0400

    add 3d model

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import CarouselBanner from "@/components/carouselBanner";
 import CarouselCategory from "@/components/carouselCategory";
 
-export default function Home() {
+export default async function Home({ params }) {
   return (
     <main className="flex flex-col">
       {/* carousel banner */}
